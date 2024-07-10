8550b9e35cf0fe98c968000e130882e48b72daa7
Aayush Pandya <166398137+Aayushpandya19@users.noreply.github.com>
2024-07-10 22:59:57 +0530

    Add files via upload

---
+++ Task-3_Calculator/app.js
@@ -0,0 +1,64 @@
+let display = document.getElementById('display');
+let currentInput = '';
+let previousInput = '';
+let operation = null;
+
+function appendNumber(number) {
+    currentInput += number;
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentInput === '') return;
+    if (previousInput !== '') {
+        calculate();
+    }
+    operation = op;
+    previousInput = currentInput;
+    currentInput = '';
+    updateDisplay();
+}
+
+function calculate() {
+    let result;
+    const prev = parseFloat(previousInput);
+    const current = parseFloat(currentInput);
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (operation) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+    currentInput = result;
+    operation = null;
+    previousInput = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentInput = '';
+    previousInput = '';
+    operation = null;
+    updateDisplay();
+}
+
+function updateDisplay() {
+    if (operation != null) {
+        display.value = `${previousInput} ${operation} ${currentInput}`;
+    } else {
+        display.value = currentInput;
+    }
+}
+
